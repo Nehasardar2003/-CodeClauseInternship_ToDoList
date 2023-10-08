ada9c0c11b612b364c2c5ca7c5d418d43eb3fae7
Nehasardar2003 <126862054+Nehasardar2003@users.noreply.github.com>
2023-10-09 00:05:39 +0530

    Create todoj.js

---
+++ todoj.js
@@ -0,0 +1,64 @@
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+var span = document.createElement("SPAN");
+var txt = document.createTextNode("\u00D7");
+span.className = "close";
+span.appendChild(txt);
+myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+}
+}
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+var li = document.createElement("li");
+var inputValue = document.getElementById("myInput").value;
+var t = document.createTextNode(inputValue);
+li.appendChild(t);
+if (inputValue === '') {
+    alert("You must write something!");
+} else {
+    document.getElementById("myUL").appendChild(li);
+    li.onclick=function(){
+        toggleChecked(this);
+    }
+}
+document.getElementById("myInput").value = "";
+
+var span = document.createElement("SPAN");
+var txt = document.createTextNode("\u00D7");
+span.className = "close";
+span.appendChild(txt);
+li.appendChild(span);
+
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+    }
+}
+}
+function showList() 
+{
+        var inputValue = document.getElementById("myInput").value;
+        var list = document.getElementById("myUL");
+
+        if (inputValue.trim() !== '') {
+            list.style.display = "block";
+        } else {
+            list.style.display = "none";
+        }
+}
+function toggleChecked(element) {
+    element.classList.toggle("checked");
+}
